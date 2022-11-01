9a4822cd6a6dcdafcc322eb6a45ef86456f80cae
sk1ly <io.skvortsov@mail.ru>
2022-11-01 23:49:52 +0300

    Упражнение по конструкторам

---
+++ src/main/kotlin/Book.kt
@@ -0,0 +1,3 @@
+package ru.sk1ly
+
+class Book(val name: String, var year: Int? = null, var price: Long? = null)
--- src/main/kotlin/ObjectOrientedProgramming.kt
+++ src/main/kotlin/ObjectOrientedProgramming.kt
@@ -6,6 +6,8 @@ fun main() {
     dog.name = "тузик"
     dog.age = 3
     dog.weight = -5
-
     println("Имя: ${dog.name}; Возраст:${dog.age} ; Вес: ${dog.weight}")
+
+    val book1 = Book("Книга 1", 1994, 499)
+    val book2 = Book("Книга 2")
 }
